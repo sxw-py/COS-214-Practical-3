c6aa8caa9f7a734c71561bdf4a9b970fa0e3aae7
sxw-py <ashmthemba@gmail.com>
2026-08-31 10:42:21 +0200

    Heinrich's intial implementation

--- Headers.h
+++ Headers.h
@@ -3,183 +3,257 @@
 
 #include <vector>
 #include <string>
+#include <iostream>
 using namespace std;
 
-enum class NoticeType {
-    OPEN,
-    CLOSE,
-    SCHEDULE_CHANGE,
-    CAPACITY_ALERT,
-    WEATHER_ALERT,
-    ESCAPED_BULL,
-    MEDICAL_EMERGENCY,
-    RESUME
+class Subject;
+
+// make composite observations relay to children
+class Observer // implement diff notifications in leaves
+{
+	public:
+		virtual void setup(Subject&) {};
+		virtual void shutdown(Subject&) {};
+		virtual void medicalEmergency(Subject&) {};
+		virtual void escapedBull(Subject&) {}; 
+		virtual void weatherAlert(Subject&) {};
+		virtual void capacityAlert(Subject&) {};
 };
 
-class AuctionNotice {
-    public:
-        NoticeType type;
-        string message;
-        int capacityThreshold;
-
-        AuctionNotice(NoticeType t, const string& msg, int threshold = 0)
-            : type(t), message(msg), capacityThreshold(threshold) {}
+class EventComponent
+{
+	public:
+		virtual void open() = 0;
+		virtual void close() = 0;
+		virtual bool reportStatus() const = 0;
+		virtual int getCapacity() const = 0;
 };
 
-class Observer {
-    public:
-        virtual ~Observer() {}
-        virtual void update(AuctionNotice* notice);
 
-    protected:
-        virtual void setupNotice(AuctionNotice* notice) {}
-        virtual void closeNotice(AuctionNotice* notice) {}
-        virtual void capacityNotice(AuctionNotice* notice) {}
-        virtual void weatherNotice(AuctionNotice* notice) {}
-        virtual void bullEscapeNotice(AuctionNotice* notice) {}
-        virtual void medicalNotice(AuctionNotice* notice) {}
-        virtual void resumeNotice(AuctionNotice* notice) {}
+class Leaf : public EventComponent, public Observer
+{
+	private:
+		int capacity;
+		bool isOpen;
+	public:
+		Leaf(int cap) : capacity(cap) {}
+		virtual void open() override
+		{
+			this->isOpen = true;
+		}
+		virtual void close() override
+		{
+			this->isOpen = false;
+		}
+		virtual bool reportStatus() const override
+		{
+			return this->isOpen;
+		}
+		virtual int getCapacity() const override
+		{
+			return this->capacity;
+		}
 };
 
-class Subject {
-    private:
-        vector<Observer*> observers;
-
-    public:
-        virtual ~Subject() {}
-        virtual void attach(Observer* observer);
-        virtual void detach(Observer* observer);
-        virtual void notify(AuctionNotice* notice);
+class Subject
+{
+	private:
+		vector<Observer*> observers;
+	public:
+		virtual void attach(Observer* o)
+		{
+			detach(o);
+			observers.push_back(o);
+		}
+		virtual void detach(Observer* observer)
+		{
+			for (auto it = observers.begin(); it != observers.end(); )
+			{
+				if (*it == observer)
+					it = observers.erase(it);
+				else
+					it++;
+			}
+		}
+		virtual void setup()
+		{
+			for (auto o : observers)
+				o->setup(*this);
+		}
+		virtual void shutdown()
+		{
+			for (auto o : observers)
+				o->shutdown(*this);
+		}
+		virtual void medicalEmergency()
+		{
+			for (auto o : observers)
+				o->medicalEmergency(*this);
+		}
+		virtual void escapedBull()
+		{
+			for (auto o : observers)
+				o->escapedBull(*this);
+		}
+		virtual void weatherAlert()
+		{
+			for (auto o : observers)
+				o->weatherAlert(*this);
+		}
+		virtual void capacityAlert()
+		{
+			for (auto o : observers)
+				o->capacityAlert(*this);
+		}
 };
 
-class EventComponent {
-    public:
-        virtual ~EventComponent() {}
-        virtual void open() = 0;
-        virtual void close() = 0;
-        virtual void reportStatus() const = 0;
-        virtual int getCapacity() const = 0;
+template <typename T>
+class Composite : public EventComponent, public Subject, public Observer
+{
+	private:
+		vector<T*> children;
+	public:
+		virtual void open() override
+		{
+			for (auto child : children)
+				child->open();
+		}
+		virtual void close() override
+		{
+			for (auto child : children)
+				child->close();
+		}
+		virtual bool reportStatus() const override
+		{
+			bool isOpen = false;
+			for (auto child : children)
+				isOpen |= child->reportStatus();
+			return isOpen; // isOpen as long as at least one child is open
+		}
+		virtual int getCapacity() const override
+		{
+			int sum = 0;
+			for (auto child : children)
+				sum += child.getCapacity();
+			return sum;
+		}
+		virtual void addChild(T* child)
+		{
+			children.push_back(child);
+		}
+
+		virtual T* removeChild(T* child)
+		{
+			// implement
+		}
+
+		virtual ~Composite()
+		{
+			// implement
+		}
 };
 
-class EventGroup : public EventComponent {
-    protected:
-        vector<EventComponent*> children;
+class Tent;
 
-    public:
-        virtual ~EventGroup();
-        virtual void add(EventComponent* child);
-        virtual void remove(EventComponent* child);
-
-        virtual void open();
-        virtual void close();
-        virtual void reportStatus() const;
-        virtual int getCapacity() const;
+class Auction : public Composite<Tent>
+{
 };
 
-class EventUnit : public EventComponent {
-    protected:
-        string name;
-        int capacity;
-        bool isOpen;
-
-    public:
-        EventUnit(const string& n, int cap) : name(n), capacity(cap), isOpen(false) {}
-        virtual ~EventUnit() {}
-
-        virtual void reportStatus() const;
-        virtual int getCapacity() const;
+class Area
+{
 };
 
-class Auction : public EventGroup, public Subject {
-    public:
-        virtual void open();
-        virtual void close();
+class Tent : public Composite<Area>
+{
 };
 
-class Tent : public EventGroup, public Observer, public Subject {
-    protected:
-        virtual void setupNotice(AuctionNotice* notice);
-        virtual void closeNotice(AuctionNotice* notice);
-        virtual void weatherNotice(AuctionNotice* notice);
-        virtual void medicalNotice(AuctionNotice* notice);
-        virtual void bullEscapeNotice(AuctionNotice* notice);
-        virtual void resumeNotice(AuctionNotice* notice);
+class Stall : public Leaf
+{
+	public:
+		Stall(int cap) : Leaf(cap) {}
 };
 
-class Area : public EventGroup, public Observer, public Subject {
-    protected:
-        virtual void setupNotice(AuctionNotice* notice);
-        virtual void closeNotice(AuctionNotice* notice);
-        virtual void weatherNotice(AuctionNotice* notice);
-        virtual void medicalNotice(AuctionNotice* notice);
-        virtual void bullEscapeNotice(AuctionNotice* notice);
-        virtual void resumeNotice(AuctionNotice* notice);
+class Complaint { public: virtual string getComplaint() = 0; };
+class Cough : public Complaint { public: virtual string getComplaint() override { return "cough"; } };
+class MI : public Complaint { public: virtual string getComplaint() override { return "Heart attack"; } };
+
+template <typename T>
+class Patient
+{
+	private:
+		string name;
+		T complaint;
+	public:
+		Patient(string name) : name(name) {}
+		virtual string getName() const { return this->name; }
+		virtual string getComplaint() const { return this->complaint->getComplaint(); }
 };
 
-class MedicalArea : public Area {
-    protected:
-        virtual void medicalNotice(AuctionNotice* notice);
-        virtual void weatherNotice(AuctionNotice* notice);
+class MedicalStall : public Stall
+{
+};
+class PulmonaryStall : public MedicalStall
+{
+	public:
+		void treatPatient(const Patient<Cough>& patient) const
+		{
+			cout << patient.getName() << " complains of " << patient.getComplaint() << endl;
+			cout << "Patient is receiving pulmonary support" << endl;
+		}
+};
+class CardiacStall : public MedicalStall
+{
+	public:
+		void treatPatient(const Patient<MI>& patient) const
+		{
+			cout << patient.getName() << " complains of " << patient.getComplaint() << endl;
+			cout << "Patient is receiving cardiac support" << endl;
+		}
 };
 
-class GeneralArea : public Area {
-    protected:
-        virtual void weatherNotice(AuctionNotice* notice);
-        virtual void bullEscapeNotice(AuctionNotice* notice);
+class MedicalArea : public Area, public Composite<MedicalStall>
+{
 };
 
-class CattleArea : public Area {
-    protected:
-        virtual void bullEscapeNotice(AuctionNotice* notice);
-        virtual void weatherNotice(AuctionNotice* notice);
-};
+class GeneralStall : public Stall 
+{
+	private:
+		int revenue;
+	public:
+		virtual int unitCost() const = 0;
+		virtual void buyFood() { revenue += unitCost(); }
+		virtual int getRevenue() { return this->revenue; }
 
-class Stall : public EventUnit, public Observer {
-    public:
-        Stall(const string& n, int cap) : EventUnit(n, cap) {}
-        virtual void open();
-        virtual void close();
 };
-
-class MedicalStall : public Stall {
-    public:
-        MedicalStall(const string& n, int cap) : Stall(n, cap) {}
-    protected:
-        virtual void medicalNotice(AuctionNotice* notice);
+class FoodStall : public GeneralStall 
+{
+	public:
+		virtual int unitCost() const override { return 30; }
 };
-
-class PulmonaryStall : public MedicalStall {
-    public:
-        PulmonaryStall(const string& n, int cap) : MedicalStall(n, cap) {}
-};
-
-class CardiacStall : public MedicalStall {
-    public:
-        CardiacStall(const string& n, int cap) : MedicalStall(n, cap) {}
-};
-
-class GeneralStall : public Stall {
-    public:
-        GeneralStall(const string& n, int cap) : Stall(n, cap) {}
-    protected:
-        virtual void weatherNotice(AuctionNotice* notice);
-};
-
-class FoodStall : public GeneralStall {
-    public:
-        FoodStall(const string& n, int cap) : GeneralStall(n, cap) {}
-};
-
 class DrinkStall : public GeneralStall {
-    public:
-        DrinkStall(const string& n, int cap) : GeneralStall(n, cap) {}
+	public:
+		virtual int unitCost() const override { return 10; }
 };
 
-class CattleStall : public Stall {
-    public:
-        CattleStall(const string& n, int cap) : Stall(n, cap) {}
-    protected:
-        virtual void bullEscapeNotice(AuctionNotice* notice);
+class GeneralArea : public Area, public Composite<GeneralStall>
+{
+};
+
+class CattleStall : public Stall 
+{
+	private:
+		int numCattle;
+	public:
+		CattleStall(int numCattle, int cap) : Stall(cap), numCattle(numCattle) {}
+		virtual int getNumCattle() { return this->numCattle; }
+		virtual void moo()
+		{
+			// print numCattle * moo
+		}
+};
+
+class CattleArea : public Area, public Composite<CattleStall>
+{
 };
 
 #endif
