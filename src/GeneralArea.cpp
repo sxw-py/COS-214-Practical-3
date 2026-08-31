916e74628d32a2d2b3d87d5fe50cbefa4e9de81b
Thando1223 <u25114469@tuks.co.za>
2026-08-31 22:34:22 +0200

    Remove file added by mistake

--- src/GeneralArea.cpp
+++
@@ -1,6 +0,0 @@
-#include "GeneralArea.h"
-
-void GeneralArea::escapedBull(Subject& s){
-    this->setSeverity(s.getSeverity());
-    this->Subject::escapedBull();  //each child stall decided how to react
-}
--- src/Tent.cpp
+++
@@ -1,8 +0,0 @@
-#include "Tent.h"
-#include <iostream>
-
-void Tent::weatherAlert(Subject& s){
-    std::cout << "Tent: activating weather protection, severity" << s.getSeverity() << std::endl;
-    this->setSeverity(s.getSeverity());   //
-    this->Subject::weatherAlert();         //relay to attached Area observers
-}
